3f21075619118cd4b21020e01c3019ff31882f39
jiHeeFlee <esder1310@naver.com>
2025-02-12 21:22:26 +0900

    feat : props 값에 따른 크기 변경 추가

--- src/shared/Box/Thumbnail.tsx
+++ src/shared/Box/Thumbnail.tsx
@@ -4,11 +4,24 @@ import styled from "@emotion/styled";
 
 interface ThumbnailProps {
     src ?: string;
+    type : 'investormain' | 'upload';
+}
+
+const SizeMap = {
+    investormain:{
+        width:'18.4375rem',
+        height:'10rem'
+    },
+    upload:{
+        // width:'25.1875rem',
+        width:'100%',
+        height:'14.125rem',
+    }
 }
 
 const Thumbnail = styled.div<ThumbnailProps>`
-    width: 18.4375rem;
-    height: 10rem;
+    width: ${({type}) => SizeMap[type].width};
+    height: ${({type}) => SizeMap[type].height};
 
     /* border-radius: 0.625rem; */
     border-radius: 0.3rem;
