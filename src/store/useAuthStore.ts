c924e12668b4708562f7fe1739e68855fba70e92
jiHeeFlee <esder1310@naver.com>
2025-02-13 16:15:06 +0900

    feat : zustand Auth State 로직 구현(임시로 localStorage)

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,34 @@
+import { create } from "zustand";
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+interface AuthState {
+    state:'login'|'logout';
+    gotoLogin:()=>void;
+    gotoLogout:()=>void;
+}
+
+/* 임시 : localStorage에서 accessToken 확인 후 초기 상태 설정 */
+const initialState:'login'|'logout' = localStorage.getItem('accessToken')?'login':'logout';
+
+const useAuthStore=create<AuthState>()(
+    persist(
+        (set)=>({
+            state:initialState,
+            gotoLogin:()=>{
+                set({state:'login'});
+                localStorage.setItem('accessToken','login'); // 예제용 토큰 저장
+            },
+            gotoLogout:()=>{
+                set({state:'logout'})
+                localStorage.removeItem('accessToken'); // 토큰 삭제
+                localStorage.removeItem('AuthState');
+            },
+        }),
+        {
+            name:'AuthState',
+            storage:createJSONStorage(()=>localStorage),
+        }
+    )
+)
+
+export default useAuthStore;
