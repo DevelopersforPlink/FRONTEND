85ef5eaffe00297c714c9f105c164cad70e67cda
jiHeeFlee <esder1310@naver.com>
2025-02-13 14:14:23 +0900

    style : favicon 변경, title 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,11 @@ import "./globals.css";
 import { LandingHeader } from "./landing/landingHeader";
 
 export const metadata: Metadata = {
-  title: "Plink",
+  title: "Plink ｜ 플러스(주) ｜ PLERS Corporation",
   description: "플링크",
-  // 로고 정해지면 주석 풀고 파비콘 추가
+  // 로고 정해지면 주석 풀고 파비콘 추가\
   icons: {
-    icon: "/logo.ico",
+    icon: "/symbol.ico",
   },
 };
 
