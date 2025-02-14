813f41b799eda49c8458ed7c1b8fc3062ccd5ea1
jiHeeFlee <esder1310@naver.com>
2025-02-15 00:23:40 +0900

    add : GNB 추가

--- src/app/investor/page.tsx
+++ src/app/investor/page.tsx
@@ -4,6 +4,7 @@
 
 import React from 'react';
 import styled from '@emotion/styled';
+import Gnb from '@/shared/Gnb';
 import Category from './components/Category';
 import StateDropDown from './components/stateDropDown';
 import Videos from './components/Videos';
@@ -12,6 +13,7 @@ export default function InvestorMain() {
   return (
     <div>
       <Container>
+        <Gnb />
         <Category />
         {/* <StateDropDown click={false} state={'테스트'}/> */}
         <Videos />
