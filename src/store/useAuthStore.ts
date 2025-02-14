9887ab9e648eeb912f4c6c303f1a8d6a06085e77
jiHeeFlee <esder1310@naver.com>
2025-02-15 00:46:10 +0900

    add : store에 type 상태 추가

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -3,17 +3,21 @@ import { persist, createJSONStorage } from 'zustand/middleware'
 
 interface AuthState {
     state:'login'|'logout';
+    type:'investor'|'founder';
     gotoLogin:()=>void;
     gotoLogout:()=>void;
 }
 
 /* 임시 : localStorage에서 accessToken 확인 후 초기 상태 설정 */
 const initialState:'login'|'logout' = localStorage.getItem('accessToken')?'login':'logout';
+/* 임시 : localStorage에서 type 확인 후 초기 상태 설정 - investor로 설정 */
+const initialType:'investor'|'founder' = localStorage.getItem('type')?'founder':'investor';
 
 const useAuthStore=create<AuthState>()(
     persist(
         (set)=>({
             state:initialState,
+            type:initialType,
             gotoLogin:()=>{
                 set({state:'login'});
                 localStorage.setItem('accessToken','login'); // 예제용 토큰 저장
