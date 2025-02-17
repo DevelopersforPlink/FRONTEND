f99efe544fe757c88fd72f722377317e018b5248
jiHeeFlee <esder1310@naver.com>
2025-02-17 18:14:28 +0900

    :memo docs : investor 컴포넌트 api 연동 준비

--- src/app/investor/components/Videos.tsx
+++ src/app/investor/components/Videos.tsx
@@ -4,42 +4,65 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import styled from "@emotion/styled";
 import InvestorMainBox from "@/shared/Box/InvestorMainComponent";
-import { boxList } from '@/constant/testVideos';
+import Image from "next/image";
+import EmptyContent from "@/shared/EmptyContent";
 
-const Videos = () => {
+interface VideosItemProps {
+    id:string;
+    thumbnail:string;
+    profile:string;
+    title : string;
+    company : string;
+    service_name : string;
+    business_type:string;
+    business_progress:string;
+    is_approve:boolean;
+    created_at:string;
+}
+
+interface VideosProps {
+    presentations:VideosItemProps[]|null;
+    onClick:()=>void;
+    type: "investor" | "founder"; 
+}
+
+const Videos:React.FC<VideosProps> = ({presentations, onClick,type}) => {
     const router = useRouter();
     return(
         <Container>
             <VideoWrapper>
-                {boxList.map(({
+                {presentations?.map(({
+                    id,
                     thumbnail,
                     profile,
                     title,
                     company,
                     service_name,
-                    id,
                     business_type,
-                    business_type_display,
-                    type
-                },)=>(
-                    <React.Fragment key={id}>
+                    business_progress,
+                    is_approve
+                },index)=>(
+                    <React.Fragment key={index}>
                         <InvestorMainBox 
+                            id={id}
                             thumbnail={thumbnail}
                             profile={profile}
                             title={title}
                             company={company} 
                             service_name={service_name}
                             business_type={business_type}
-                            business_type_display={business_type_display}
+                            business_progress={business_progress}
                             
-                            key={id}
+                            is_approve={is_approve}
                             onClick={()=> router.push(`/investor/${id}`)}
                             type={type}
-                            id={id}
                             />
                     </React.Fragment>
                 ))}
             </VideoWrapper>
+            {presentations === null &&         
+                <EmptyContent type='category'/>
+            }
         </Container>
     )
 };
@@ -51,14 +74,16 @@ const Container = styled.div`
     flex-direction: column;
     
     margin-top: 3rem;
-    gap: 1.5rem;
+    /* gap: 1.5rem; */
     width: 100%;
 `;
 
 const VideoWrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(4, 1fr); // ✅ 1줄에 4개 
-    grid-column-gap: 1.5rem; // ✅ 간격 3개 유지
+    grid-column-gap: 2rem; // ✅ 간격 3개 유지
     grid-row-gap: 1.5rem;
     place-items: center;
+
+    /* background-color: yellow; */
 `;
